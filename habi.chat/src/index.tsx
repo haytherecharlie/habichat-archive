1152f9702e6d125d82869f13585720ca634432aa
charliehay <charlie.hay@telus.com>
2019-09-19 09:49:44 -0400

    feat(hc): init datalater

--- habi.chat/src/index.tsx
+++ habi.chat/src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Router from 'routes/Router'
+import { initDataLayer } from 'utils/analytics'
 import * as workers from 'utils/workers'
+initDataLayer()
 
 ReactDOM.render(<Router />, document.getElementById('root'))
 workers.unregister()
