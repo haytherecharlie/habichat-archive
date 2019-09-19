b3cb4d4139e27baeceb9950b83c67fe8cf9729aa
charliehay <charlie.hay@telus.com>
2019-09-19 09:36:49 -0400

    feat(hc): hello@habi.chat

--- habi.chat/src/components/Footer/Footer.component.tsx
+++ habi.chat/src/components/Footer/Footer.component.tsx
@@ -4,7 +4,8 @@ import * as S from './Footer.style'
 const Footer = () => {
   return (
     <S.Footer>
-      <S.Title>&copy; 2019 Habichat</S.Title>
+      <S.Title>Email Us: hello@habi.chat</S.Title>
+      <S.Copyright>&copy; 2019 Habichat</S.Copyright>
     </S.Footer>
   )
 }
--- habi.chat/src/components/Footer/Footer.style.ts
+++ habi.chat/src/components/Footer/Footer.style.ts
@@ -3,10 +3,17 @@ import styled from 'styled-components'
 export const Footer = styled('div')({
   flex: 1,
   display: 'flex',
+  flexDirection: 'column',
   alignItems: 'center',
   justifyContent: 'center'
 })
 
 export const Title = styled('span')({
-  fontSize: 13
+  fontSize: 15,
+  marginBottom: '1rem'
+})
+
+export const Copyright = styled('span')({
+  fontSize: 13,
+  marginBottom: '1rem'
 })
