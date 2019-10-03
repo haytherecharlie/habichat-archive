7759dda5dd86cbf2ca7ff8e0bc051403d9a61d62
charliehay <charlie.hay@telus.com>
2019-10-03 08:56:50 -0400

    feat(hc): router firebase connect

--- native/src/routes/Router/Router.component.jsx
+++ native/src/routes/Router/Router.component.jsx
@@ -1,26 +1,25 @@
-import React from 'react'
-import { createAppContainer } from 'react-navigation'
-import { createStackNavigator } from 'react-navigation-stack'
-import Community from 'src/screens/Community'
-// import types from 'prop-types'
-import * as S from './Router.style'
+import React, { useEffect } from 'react'
+import types from 'prop-types'
+import { documentListener } from 'src/services/firebase'
 
-const Stack = createAppContainer(
-  createStackNavigator(
-    {
-      Community
-    },
-    {
-      initialRouteName: 'Community'
-    }
-  )
-)
+const Router = ({ Stack, subAccount, unsubAccount, subCommunity, unsubCommunity }) => {
+  useEffect(() => {
+    documentListener('account', subAccount)
+    documentListener('community', subCommunity)
+    return () => Promise.all([unsubAccount(), unsubCommunity()])
+  }, [subAccount, unsubAccount, subCommunity, unsubCommunity])
 
-const Router = () => {
   return <Stack />
 }
 
 Router.defaultProps = {}
-Router.propTypes = {}
+
+Router.propTypes = {
+  Stack: types.func.isRequired,
+  subAccount: types.func.isRequired,
+  unsubAccount: types.func.isRequired,
+  subCommunity: types.func.isRequired,
+  unsubCommunity: types.func.isRequired
+}
 
 export default Router
--- native/src/routes/Router/Router.container.js
+++ native/src/routes/Router/Router.container.js
@@ -1,11 +1,29 @@
-import { connect } from 'react-redux'
 import { path } from 'ramda'
+import { connect } from 'react-redux'
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import { subAccount, unsubAccount, subCommunity, unsubCommunity } from 'src/services/redux'
+import Community from 'src/screens/Community'
 import Router from './Router.component'
 
+export const Stack = createAppContainer(
+  createStackNavigator({ Community }, { initialRouteName: 'Community' })
+)
+
 export const state = (state) => {
-  return {}
+  return {
+    Stack
+  }
 }
 
-export const dispatch = {}
+export const dispatch = {
+  subAccount,
+  unsubAccount,
+  subCommunity,
+  unsubCommunity
+}
 
-export default connect(state, dispatch)(Router)
+export default connect(
+  state,
+  dispatch
+)(Router)
