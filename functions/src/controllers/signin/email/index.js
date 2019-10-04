834e6a81da0fc2f67f7484188450049f2eb3e2c8
charliehay <charlie.hay@telus.com>
2019-10-04 16:30:23 -0400

    feat(hc): github banner

--- functions/src/controllers/signin/email/index.js
+++ functions/src/controllers/signin/email/index.js
@@ -1,4 +1,5 @@
 import { https } from 'firebase-functions'
+import { signCustomToken } from 'services/firebase'
 
 const helloWorld = https.onRequest((request, response) => {
   response.send('shoot from Firebase!')
